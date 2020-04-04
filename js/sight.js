36c0d5075f521a3427190c5efb7bb7f763ff714d
kabeerchhillar <kabeer.chhillar@gmail.com>
2020-04-04 16:24:29 +0530

    Completed module

--- js/sight.js
+++ js/sight.js
@@ -1,13 +1,13 @@
 class SVGElement { 
 	constructor ( type ) { 
 		this.type = type;
-		this.namespace = "http://www.w3.org/2000/svg";
+		this.namespace = 'http://www.w3.org/2000/svg';
 		this.node = document.createElementNS(this.namespace, this.type);
-		retun this;
+		return this;
 	}
+  	
+	attr(attrs) { 
 	
-	attrs(attrs) { 
-		
 		for (const [key, value] of Object.entries(attrs)) {
       		this.node.setAttributeNS(null, key, value);
     	}
@@ -16,15 +16,9 @@ class SVGElement {
 	}
 	
 	append(element){
-		
-		if(typeof element == 'string')
-			const parent = document.querySelector(element);
-		else
-			const parent = element.node;
-		
-		parent.appendChild(this.node);
-	
-		return this;
+  	  	const parent = (typeof element === 'string') ? document.querySelector(element) : element.node;
+   		 parent.appendChild(this.node);
+   		 return this;
 	}
 }
 
